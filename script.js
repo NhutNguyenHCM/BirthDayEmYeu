7d7095fc8abae444e416c37e366b9cfdb404ed83
Nguyen Khanh Nhut <nhutnguyen@ykns6.onmicrosoft.com>
2023-08-04 21:54:45 +0700

    auto

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ Reveal.addEventListener('slidechanged', function (event) {
     // Get the current slide and its video element(s)
     var currentSlide = event.currentSlide;
     var videos = currentSlide.querySelectorAll('video');
+    var audios = currentSlide.querySelectorAll('audio');
 
     // Play the video(s) when the slide becomes visible
-    videos.forEach(function (video) {
-      if (!video.hasAttribute('data-played')) {
+    [...videos,...audios].forEach(function (video) {
+      if (!video.closest('present')) {
         video.play();
-        video.setAttribute('data-played', 'true');
+        // video.setAttribute('data-played', 'true');
       }
     });
   });
