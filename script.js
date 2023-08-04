e9c336329b60276e597a7e380045b00c4965c52e
Nguyen Khanh Nhut <nhutnguyen@ykns6.onmicrosoft.com>
2023-08-04 22:06:39 +0700

    auto1

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
 // Add an event listener to handle when a slide becomes visible
 Reveal.addEventListener('slidechanged', function (event) {
+    // Get the previous slide and its video element(s)
+    var previousSlide = event.previousSlide;
+    var previousVideos = previousSlide.querySelectorAll('video');
+
+    // Pause the videos of the previous slide when it's no longer visible
+    previousVideos.forEach(function (video) {
+      video.pause();
+      video.removeAttribute('data-played');
+    });
+
     // Get the current slide and its video element(s)
     var currentSlide = event.currentSlide;
     var videos = currentSlide.querySelectorAll('video');
-    var audios = currentSlide.querySelectorAll('audio');
 
-    // Play the video(s) when the slide becomes visible
-    [...videos,...audios].forEach(function (video) {
-      if (!video.closest('present')) {
-        video.play();
-        // video.setAttribute('data-played', 'true');
-      }
+    // Function to handle autoplay of videos
+    function handleVideoAutoplay(entries) {
+      entries.forEach((entry) => {
+        const video = entry.target;
+        if (entry.isIntersecting && !video.hasAttribute('data-played')) {
+          // Add a delay before playing the video
+          setTimeout(() => {
+            // Check if the video is still in the viewport
+            if (entry.isIntersecting) {
+              video.play().then(() => {
+                video.setAttribute('data-played', 'true');
+              }).catch((error) => {
+                console.error('Error playing the video:', error);
+              });
+            }
+          }, 500); // Adjust the delay as needed
+        }
+      });
+    }
+
+    // Set up the Intersection Observer for video elements
+    const observerOptions = {
+      root: null,
+      threshold: 0.2, // Adjust the threshold as needed for when the video should start playing
+    };
+
+    const videoObserver = new IntersectionObserver(handleVideoAutoplay, observerOptions);
+
+    videos.forEach((video) => {
+      videoObserver.observe(video);
     });
   });
-
-
-
-
-
